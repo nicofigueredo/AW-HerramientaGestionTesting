93b57080916abfff9bfde618e07d10c10c71b997
nicofigueredo <nico_1507@live.com.ar>
2019-06-12 20:19:15 -0300

    Se agregan los archivos recursivamente

--- src/unlam/edu/herramienta/HerramientaTesting.java
+++ src/unlam/edu/herramienta/HerramientaTesting.java
@@ -58,7 +58,7 @@ public class HerramientaTesting extends JFrame {
 	private JList<String> classesList;
 	private JList<String> methodsList;
 	private TextArea codeTextPane;
-	
+
 	private JLabel lblLineasDeCodigoResult;
 	private JLabel lblLineasDeCodigoComentadasResult;
 	private JLabel lblPorcentajeDeLineasDeCodigoComentadasResult;
@@ -67,26 +67,25 @@ public class HerramientaTesting extends JFrame {
 	private JLabel lblFanOutResult;
 	private JLabel lblHalsteadLongitudResult;
 	private JLabel lblHalsteadVolumenResult;
-	
-	
+
 	public void initMenu() {
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
-		
+
 		JMenu mnAnalisis = new JMenu("Analisis");
 		menuBar.add(mnAnalisis);
-		
+
 		JMenuItem mntmElegirCarpeta = new JMenuItem("Elegir Carpeta");
 		mntmElegirCarpeta.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, InputEvent.CTRL_MASK));
 		mntmElegirCarpeta.addActionListener(cargarArchivos);
-		
+
 		mnAnalisis.add(mntmElegirCarpeta);
-		
+
 		JMenuItem mntmSalirCtrl = new JMenuItem("Salir");
 		mntmSalirCtrl.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK));
 		mnAnalisis.add(mntmSalirCtrl);
 	}
-	
+
 	public void initMetricLabels() {
 		JPanel panel = new JPanel();
 		panel.setOpaque(false);
@@ -94,198 +93,219 @@ public class HerramientaTesting extends JFrame {
 		panel.setBounds(553, 36, 258, 362);
 		contentPane.add(panel);
 		panel.setLayout(null);
-		
+
 		JLabel lblLineasDeCodigoTotales = new JLabel("Lineas de codigo totales:");
 		lblLineasDeCodigoTotales.setHorizontalAlignment(SwingConstants.CENTER);
 		lblLineasDeCodigoTotales.setBounds(10, 11, 238, 14);
 		panel.add(lblLineasDeCodigoTotales);
-		
+
 		JLabel lblLineasDeCodigoComentadas = new JLabel("Lineas de codigo comentadas:");
 		lblLineasDeCodigoComentadas.setHorizontalAlignment(SwingConstants.CENTER);
 		lblLineasDeCodigoComentadas.setBounds(10, 51, 238, 14);
 		panel.add(lblLineasDeCodigoComentadas);
-		
+
 		JLabel lblPorcentajeDeLinea = new JLabel("Porcentaje de lineas de codigo comentadas:");
 		lblPorcentajeDeLinea.setHorizontalAlignment(SwingConstants.CENTER);
 		lblPorcentajeDeLinea.setBounds(10, 91, 238, 14);
 		panel.add(lblPorcentajeDeLinea);
-		
+
 		JLabel lblComplejidadCiclomatica = new JLabel("Complejidad ciclomatica:");
 		lblComplejidadCiclomatica.setHorizontalAlignment(SwingConstants.CENTER);
 		lblComplejidadCiclomatica.setBounds(10, 131, 238, 14);
 		panel.add(lblComplejidadCiclomatica);
-		
+
 		JLabel lblFanIn = new JLabel("Fan in:");
 		lblFanIn.setHorizontalAlignment(SwingConstants.CENTER);
 		lblFanIn.setBounds(10, 171, 238, 14);
 		panel.add(lblFanIn);
-		
+
 		JLabel lblFanOut = new JLabel("Fan out:");
 		lblFanOut.setHorizontalAlignment(SwingConstants.CENTER);
 		lblFanOut.setBounds(10, 211, 238, 14);
 		panel.add(lblFanOut);
-		
+
 		JLabel lblHalsteadLongitud = new JLabel("Halstead longitud:");
 		lblHalsteadLongitud.setHorizontalAlignment(SwingConstants.CENTER);
 		lblHalsteadLongitud.setBounds(10, 251, 238, 14);
 		panel.add(lblHalsteadLongitud);
-		
+
 		JLabel lblHalsteadVolumen = new JLabel("Halstead volumen:");
 		lblHalsteadVolumen.setHorizontalAlignment(SwingConstants.CENTER);
 		lblHalsteadVolumen.setBounds(10, 291, 238, 14);
 		panel.add(lblHalsteadVolumen);
-		
+
 		lblLineasDeCodigoResult = new JLabel("");
 		lblLineasDeCodigoResult.setHorizontalAlignment(SwingConstants.CENTER);
 		lblLineasDeCodigoResult.setBounds(10, 31, 238, 14);
 		panel.add(lblLineasDeCodigoResult);
-		
+
 		lblLineasDeCodigoComentadasResult = new JLabel("");
 		lblLineasDeCodigoComentadasResult.setHorizontalAlignment(SwingConstants.CENTER);
 		lblLineasDeCodigoComentadasResult.setBounds(10, 71, 238, 14);
 		panel.add(lblLineasDeCodigoComentadasResult);
-		
+
 		lblPorcentajeDeLineasDeCodigoComentadasResult = new JLabel("");
 		lblPorcentajeDeLineasDeCodigoComentadasResult.setHorizontalAlignment(SwingConstants.CENTER);
 		lblPorcentajeDeLineasDeCodigoComentadasResult.setBounds(10, 111, 238, 14);
 		panel.add(lblPorcentajeDeLineasDeCodigoComentadasResult);
-		
+
 		lblComplejidadCiclomaticaResult = new JLabel("");
 		lblComplejidadCiclomaticaResult.setHorizontalAlignment(SwingConstants.CENTER);
 		lblComplejidadCiclomaticaResult.setBounds(10, 151, 238, 14);
 		panel.add(lblComplejidadCiclomaticaResult);
-		
+
 		lblFanInResult = new JLabel("");
 		lblFanInResult.setHorizontalAlignment(SwingConstants.CENTER);
 		lblFanInResult.setBounds(10, 191, 238, 14);
 		panel.add(lblFanInResult);
-		
+
 		lblFanOutResult = new JLabel("");
 		lblFanOutResult.setHorizontalAlignment(SwingConstants.CENTER);
 		lblFanOutResult.setBounds(10, 231, 238, 14);
 		panel.add(lblFanOutResult);
-		
+
 		lblHalsteadLongitudResult = new JLabel("");
 		lblHalsteadLongitudResult.setHorizontalAlignment(SwingConstants.CENTER);
 		lblHalsteadLongitudResult.setBounds(10, 271, 238, 14);
 		panel.add(lblHalsteadLongitudResult);
-		
+
 		lblHalsteadVolumenResult = new JLabel("");
 		lblHalsteadVolumenResult.setHorizontalAlignment(SwingConstants.CENTER);
 		lblHalsteadVolumenResult.setBounds(10, 311, 238, 14);
 		panel.add(lblHalsteadVolumenResult);
 	}
-	
-	public HerramientaTesting() throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
-		
+
+	public HerramientaTesting() throws ClassNotFoundException, InstantiationException, IllegalAccessException,
+			UnsupportedLookAndFeelException {
+
 		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		setTitle("Herramienta de Gestion de Testing");
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(0, 0, 850, 690);
 		setLocationRelativeTo(null);
-		
+
 		initMenu();
-		
+
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		initMetricLabels();
-		
+
 		JLabel lblNewLabel = new JLabel("Seleccione un archivo de la lista");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblNewLabel.setForeground(new Color(0, 0, 128));
 		lblNewLabel.setBounds(10, 11, 197, 14);
 		contentPane.add(lblNewLabel);
-		
+
 		JScrollPane filesScrollPane = new JScrollPane();
 		filesScrollPane.setBounds(10, 36, 521, 99);
 		contentPane.add(filesScrollPane);
-		
+
 		filesList = new JList<>();
 		filesList.addMouseListener(cargarClases);
 		filesScrollPane.setViewportView(filesList);
-		
+
 		JLabel lblSeleccioneUnaClase = new JLabel("Seleccione una clase de la lista");
 		lblSeleccioneUnaClase.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblSeleccioneUnaClase.setForeground(new Color(0, 0, 128));
 		lblSeleccioneUnaClase.setBounds(22, 146, 185, 14);
 		contentPane.add(lblSeleccioneUnaClase);
-		
+
 		JScrollPane classesScrollPane = new JScrollPane();
 		classesScrollPane.setBounds(10, 171, 258, 202);
 		contentPane.add(classesScrollPane);
-		
+
 		classesList = new JList<>();
 		classesList.addMouseListener(cargarMetodos);
 		classesScrollPane.setViewportView(classesList);
-		
+
 		JScrollPane methodsScrollPane = new JScrollPane();
 		methodsScrollPane.setBounds(278, 171, 253, 202);
 		contentPane.add(methodsScrollPane);
-		
-		methodsList  = new JList<>();
+
+		methodsList = new JList<>();
 		methodsList.addMouseListener(cargarCodigoYmetricas);
 		methodsScrollPane.setViewportView(methodsList);
-		
+
 		JLabel lblSeleccioneUnMetodo = new JLabel("Seleccione un metodo de la lista");
 		lblSeleccioneUnMetodo.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblSeleccioneUnMetodo.setForeground(new Color(0, 0, 128));
 		lblSeleccioneUnMetodo.setBounds(278, 146, 185, 14);
 		contentPane.add(lblSeleccioneUnMetodo);
-		
+
 		JLabel lblCodigoDelMetodo = new JLabel("Codigo del metodo seleccionado");
 		lblCodigoDelMetodo.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblCodigoDelMetodo.setForeground(new Color(0, 0, 128));
 		lblCodigoDelMetodo.setBounds(10, 384, 197, 14);
 		contentPane.add(lblCodigoDelMetodo);
-		
+
 		JScrollPane codePane = new JScrollPane();
 		codePane.setBounds(10, 409, 774, 220);
 		contentPane.add(codePane);
-		
+
 		codeTextPane = new TextArea();
 		codeTextPane.setBackground(Color.WHITE);
 		codeTextPane.setEditable(false);
 		codePane.setViewportView(codeTextPane);
-		
+
 		JLabel lblAnalisisDelMetodo = new JLabel("Analisis del metodo");
 		lblAnalisisDelMetodo.setBackground(new Color(240, 240, 240));
 		lblAnalisisDelMetodo.setForeground(new Color(0, 0, 128));
 		lblAnalisisDelMetodo.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblAnalisisDelMetodo.setBounds(565, 4, 115, 14);
 		contentPane.add(lblAnalisisDelMetodo);
-		
-		
+
 	}
-	
+
 	ActionListener cargarArchivos = new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
-			fileChooser = new JFileChooser();		
+			fileChooser = new JFileChooser();
 			fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 			int fileChooserResult = fileChooser.showOpenDialog(null);
-			if(fileChooserResult == JFileChooser.APPROVE_OPTION) {
+			if (fileChooserResult == JFileChooser.APPROVE_OPTION) {
 				File carpeta = fileChooser.getSelectedFile();
-				File[] archivos = carpeta.listFiles(new FilenameFilter() {
-			        @Override
-			        public boolean accept(File dir, String name) {
-			            return name.toLowerCase().endsWith(".java");
-			        }
-			    });
-		        DefaultListModel<String> filenames = new DefaultListModel<>();			
 				
-				for(File file: archivos) {				
+				// File[] archivos = carpeta.listFiles(javaFilter);
+				List<File> archivos = listAllSubfiles(carpeta);
+				
+				DefaultListModel<String> filenames = new DefaultListModel<>();
+				for (File file : archivos) {
 					filenames.addElement(file.getAbsolutePath());
-				}			
-				filesList.setModel(filenames);						
+				}
+				filesList.setModel(filenames);
 			}
 		}
 	};
-	
+
+	FilenameFilter javaFilter = new FilenameFilter() {
+		@Override
+		public boolean accept(File dir, String name) {
+			return dir.isDirectory() || name.toLowerCase().endsWith(".java");
+		}
+	};
+
+	public List<File> listAllSubfiles(File source) {
+		List<File> lists = new ArrayList<File>();
+		addSubFilesToList(lists, source);
+		return lists;
+	}
+
+	private void addSubFilesToList(List<File> list, File source) {
+		File[] files = source.listFiles(javaFilter);
+		if(files == null || files.length == 0)
+			return;
+		for (File file : files) {
+			if(file.isFile())
+				list.add(file);
+			addSubFilesToList(list, file);
+		}
+	}
+
 	private List<Clase> clasesProyecto;
-	
+
 	MouseListener cargarClases = new MouseAdapter() {
 		@Override
 		public void mouseClicked(MouseEvent e) {
@@ -296,107 +316,109 @@ public class HerramientaTesting extends JFrame {
 			DefaultListModel<String> classnames = new DefaultListModel<>();
 			for (int indice = 0; indice < clasesProyecto.size(); indice++) {
 				classnames.addElement(clasesProyecto.get(indice).getNombre());
-            }
+			}
 			classesList.setModel(classnames);
 		}
 	};
-	
+
 	private List<Metodo> metodosClaseElegida;
-	
+
 	MouseListener cargarMetodos = new MouseAdapter() {
 		@Override
 		public void mouseClicked(MouseEvent e) {
 			if (classesList.getSelectedIndex() == -1)
 				return;
 			Integer claseSeleccionada = classesList.getSelectedIndex();
-            Clase claseElegida = clasesProyecto.get(claseSeleccionada);
-            metodosClaseElegida = claseElegida.getMetodos();
-            
+			Clase claseElegida = clasesProyecto.get(claseSeleccionada);
+			metodosClaseElegida = claseElegida.getMetodos();
+
 			DefaultListModel<String> methodnames = new DefaultListModel<>();
 			for (int indice = 0; indice < metodosClaseElegida.size(); indice++) {
 				methodnames.addElement(metodosClaseElegida.get(indice).getNombre());
-            }
+			}
 			methodsList.setModel(methodnames);
 		}
 	};
-	
+
 	public List<ResultadoMetrica> calcularMetricas(Metodo metodo) {
-        List<ResultadoMetrica> resultados = new ArrayList<ResultadoMetrica>();
-        List<Metrica> metricas = new ArrayList<Metrica>();
-        metricas.add(new ComplejidadCiclomatica());
-        metricas.add(new CantidadLineas());
-        metricas.add(new CantidadComentarios());
-        metricas.add(new Halstead());
-        metricas.add(new FanIn(clasesProyecto));
-        metricas.add(new FanOut(clasesProyecto));
+		List<ResultadoMetrica> resultados = new ArrayList<ResultadoMetrica>();
+		List<Metrica> metricas = new ArrayList<Metrica>();
+		metricas.add(new ComplejidadCiclomatica());
+		metricas.add(new CantidadLineas());
+		metricas.add(new CantidadComentarios());
+		metricas.add(new Halstead());
+		metricas.add(new FanIn(clasesProyecto));
+		metricas.add(new FanOut(clasesProyecto));
 
-        for (Metrica metrica : metricas) {
-            metrica.calcular(metodo);
-            resultados.add(metrica.obtenerResultado());
-        }
-        return resultados;
-    }
+		for (Metrica metrica : metricas) {
+			metrica.calcular(metodo);
+			resultados.add(metrica.obtenerResultado());
+		}
+		return resultados;
+	}
 
-	
-    MouseListener cargarCodigoYmetricas = new MouseAdapter() {
+	MouseListener cargarCodigoYmetricas = new MouseAdapter() {
 
-        public void mouseClicked(MouseEvent e) {
-        	
-        	if (methodsList.getSelectedIndex() == -1) 
-                return;
-            
-            Integer metodoSeleccionado = methodsList.getSelectedIndex();            
-            Metodo metodoElegido = metodosClaseElegida.get(metodoSeleccionado);
-            List<ResultadoMetrica> resultados = calcularMetricas(metodoElegido);
+		public void mouseClicked(MouseEvent e) {
 
-            new Thread(new TextAreaUpdater(codeTextPane, metodoElegido.getCodigo())).start();
+			if (methodsList.getSelectedIndex() == -1)
+				return;
 
-            //Aca hay un problema, si cambiamos el orden de resolucion de las metricas esto tambien hay que cambiarlo
-            /**
-             * Complejidad Ciclomatica
-             */
-            Integer complejidadCiclomatica = Integer.parseInt(resultados.get(0).getResultado());
-            if (complejidadCiclomatica > 10) {
-                lblComplejidadCiclomaticaResult.setForeground(Color.RED);
-                lblComplejidadCiclomaticaResult.setToolTipText("La complejidad ciclom\u00E1tica supera 10, es recomendable modularizar el m\u00E9todo.");
-            } else {
-            	lblComplejidadCiclomaticaResult.setForeground(Color.DARK_GRAY);
-            	lblComplejidadCiclomaticaResult.setToolTipText(null);
-            }
-            lblComplejidadCiclomaticaResult.setText(complejidadCiclomatica.toString());
+			Integer metodoSeleccionado = methodsList.getSelectedIndex();
+			Metodo metodoElegido = metodosClaseElegida.get(metodoSeleccionado);
+			List<ResultadoMetrica> resultados = calcularMetricas(metodoElegido);
 
-            /**
-             * Lineas de codigo, comentarios y porcentaje de comentarios
-             */
-            lblLineasDeCodigoResult.setText(resultados.get(1).getResultado());
-            lblLineasDeCodigoComentadasResult.setText(resultados.get(2).getResultado());
+			new Thread(new TextAreaUpdater(codeTextPane, metodoElegido.getCodigo())).start();
 
-            Double porcentajeComentarios = Integer.parseInt(lblLineasDeCodigoComentadasResult.getText()) * 100.0 / Integer.parseInt(lblLineasDeCodigoResult.getText());
+			// Aca hay un problema, si cambiamos el orden de resolucion de las metricas esto
+			// tambien hay que cambiarlo
+			/**
+			 * Complejidad Ciclomatica
+			 */
+			Integer complejidadCiclomatica = Integer.parseInt(resultados.get(0).getResultado());
+			if (complejidadCiclomatica > 10) {
+				lblComplejidadCiclomaticaResult.setForeground(Color.RED);
+				lblComplejidadCiclomaticaResult.setToolTipText(
+						"La complejidad ciclom\u00E1tica supera 10, es recomendable modularizar el m\u00E9todo.");
+			} else {
+				lblComplejidadCiclomaticaResult.setForeground(Color.DARK_GRAY);
+				lblComplejidadCiclomaticaResult.setToolTipText(null);
+			}
+			lblComplejidadCiclomaticaResult.setText(complejidadCiclomatica.toString());
 
-            if (porcentajeComentarios < 15) {
-                lblPorcentajeDeLineasDeCodigoComentadasResult.setForeground(Color.RED);
-                lblPorcentajeDeLineasDeCodigoComentadasResult.setToolTipText("El porcentaje de comentarios recomendable es del 15%. Agregue m\u00E1s comentarios al m\u00E9todo.");
-            } else {
-            	lblPorcentajeDeLineasDeCodigoComentadasResult.setForeground(Color.DARK_GRAY);
-            	lblPorcentajeDeLineasDeCodigoComentadasResult.setToolTipText(null);
-            }
-            lblPorcentajeDeLineasDeCodigoComentadasResult.setText(String.format("%.2f", porcentajeComentarios) + "%");
+			/**
+			 * Lineas de codigo, comentarios y porcentaje de comentarios
+			 */
+			lblLineasDeCodigoResult.setText(resultados.get(1).getResultado());
+			lblLineasDeCodigoComentadasResult.setText(resultados.get(2).getResultado());
 
-            /**
-             * Halstead
-             */
-            String halstead[] = resultados.get(3).getResultado().split(" ");
-            lblHalsteadLongitudResult.setText(halstead[1]);
-            lblHalsteadVolumenResult.setText(halstead[3]);
+			Double porcentajeComentarios = Integer.parseInt(lblLineasDeCodigoComentadasResult.getText()) * 100.0
+					/ Integer.parseInt(lblLineasDeCodigoResult.getText());
 
-            /**
-             * Fan-In y Fan-Out
-             */
-            lblFanInResult.setText(resultados.get(4).getResultado());
-            lblFanOutResult.setText(resultados.get(5).getResultado());
+			if (porcentajeComentarios < 15) {
+				lblPorcentajeDeLineasDeCodigoComentadasResult.setForeground(Color.RED);
+				lblPorcentajeDeLineasDeCodigoComentadasResult.setToolTipText(
+						"El porcentaje de comentarios recomendable es del 15%. Agregue m\u00E1s comentarios al m\u00E9todo.");
+			} else {
+				lblPorcentajeDeLineasDeCodigoComentadasResult.setForeground(Color.DARK_GRAY);
+				lblPorcentajeDeLineasDeCodigoComentadasResult.setToolTipText(null);
+			}
+			lblPorcentajeDeLineasDeCodigoComentadasResult.setText(String.format("%.2f", porcentajeComentarios) + "%");
 
-        }
-    };	
-	
-	
+			/**
+			 * Halstead
+			 */
+			String halstead[] = resultados.get(3).getResultado().split(" ");
+			lblHalsteadLongitudResult.setText(halstead[1]);
+			lblHalsteadVolumenResult.setText(halstead[3]);
+
+			/**
+			 * Fan-In y Fan-Out
+			 */
+			lblFanInResult.setText(resultados.get(4).getResultado());
+			lblFanOutResult.setText(resultados.get(5).getResultado());
+
+		}
+	};
+
 }
